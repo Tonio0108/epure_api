def42aaae3ff9929983ea6fa1fe36355bb1100ca
tonio0108 <tonioandria2@gmail.com>
2026-02-05 20:35:59 +0300

    update migrations

--- prisma/migrations/20260205173531_migration_pg/migration.sql
+++ prisma/migrations/20260205173531_migration_pg/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "Analysis" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "jobTitle" TEXT NOT NULL,
     "candidateName" TEXT,
     "score" INTEGER NOT NULL,
     "missingSkills" TEXT NOT NULL,
     "suggestions" TEXT NOT NULL,
-    "rawText" TEXT NOT NULL
+    "rawText" TEXT NOT NULL,
+
+    CONSTRAINT "Analysis_pkey" PRIMARY KEY ("id")
 );
